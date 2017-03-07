262bc221f611ff1f7da2c5aa389dac0eeb779409
Kan Huang <kan.huang@gmail.com>
2017-03-07 08:09:07 -0800

    Finished

--- ListsAndMoreLists/Program.cs
+++ ListsAndMoreLists/Program.cs
@@ -10,6 +10,39 @@ namespace ListsAndMoreLists
     {
         static void Main(string[] args)
         {
+            //Initialize a new instance of user list
+            List<User> users = new List<User>();
+            users.Add(new User { Name = "Dave", Password = "hello" });
+            users.Add(new User { Name = "Steve", Password = "steve" });
+            users.Add(new User { Name = "Lisa", Password = "hello" });
+            
+
+            //Display to the console, all the passwords that are "hello". Hint: Where
+            List<User> helloList = (from x in users
+                            where x.Password == "hello"
+                            select x).ToList();
+            
+            Console.WriteLine("The Following User has password as \"hello\"");
+            helloList.AsParallel().ForAll(s => Console.WriteLine(s.Name));
+
+            //Delete any passwords that are the lower-cased version of their Name. Do not just look for "steve". It has to be data-driven. Hint: Remove
+            users.RemoveAll(x => x.Password == x.Name.ToLower());
+            
+            
+            //Delete the first User that has the password "hello". Hint: First or FirstOrDefault
+            var firstHello = (from x in users
+                                    where x.Password == "hello"
+                                    select x).FirstOrDefault();
+            if(firstHello != null)
+            {
+                users.Remove(firstHello);
+            }
+
+            //Display to the console the remaining users with their Name and Password.
+            Console.WriteLine("Here are remaining users");
+            users.AsParallel().ForAll(s => Console.WriteLine(s.Name));
+
         }
+        
     }
 }
